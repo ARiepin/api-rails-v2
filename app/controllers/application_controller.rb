2bbe5eb0b9762e2dce957628d98d95fb87fe16ea
Andrii Riepin <a.ryepin@svitla.com>
2014-10-17 17:51:36 +0300

    users, songs and playlists with serializers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+	include ActionController::HttpAuthentication::Token::ControllerMethods
+	include ActionController::MimeResponds
+	include ActionController::Serialization
+	
+
+	before_filter :authenticate_user_from_token!
+	# before_filter :authenticate_user!
+
+	def authenticate_user_from_token!
+    user_token = params[:user_token].presence
+    user       = user_token && User.find_by_authentication_token(user_token.to_s)
+ 
+    if user
+    	sign_in user, store: false
+    end
+  end
 end
